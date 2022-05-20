d1f24e7b03b00af2a6c08ed7833c49dca6f81aee
LilyYeh <lilyyeh@core-tech.tw>
2022-05-20 16:44:42 +0800

    modify some text laravel8 to laravel9

--- public/js/productDetail.js
+++ public/js/productDetail.js
@@ -45200,13 +45200,13 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 /* harmony import */ var _deal_vue_vue_type_template_id_83a00d92__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./deal.vue?vue&type=template&id=83a00d92 */ "./resources/js/components/deal.vue?vue&type=template&id=83a00d92");
 /* harmony import */ var _deal_vue_vue_type_script_lang_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./deal.vue?vue&type=script&lang=js */ "./resources/js/components/deal.vue?vue&type=script&lang=js");
-/* harmony import */ var _Users_core_Documents_project_laravel8_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./node_modules/vue-loader/dist/exportHelper.js */ "./node_modules/vue-loader/dist/exportHelper.js");
+/* harmony import */ var _Users_core_Documents_project_laravel9_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./node_modules/vue-loader/dist/exportHelper.js */ "./node_modules/vue-loader/dist/exportHelper.js");
 
 
 
 
 ;
-const __exports__ = /*#__PURE__*/(0,_Users_core_Documents_project_laravel8_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_2__["default"])(_deal_vue_vue_type_script_lang_js__WEBPACK_IMPORTED_MODULE_1__["default"], [['render',_deal_vue_vue_type_template_id_83a00d92__WEBPACK_IMPORTED_MODULE_0__.render],['__file',"resources/js/components/deal.vue"]])
+const __exports__ = /*#__PURE__*/(0,_Users_core_Documents_project_laravel9_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_2__["default"])(_deal_vue_vue_type_script_lang_js__WEBPACK_IMPORTED_MODULE_1__["default"], [['render',_deal_vue_vue_type_template_id_83a00d92__WEBPACK_IMPORTED_MODULE_0__.render],['__file',"resources/js/components/deal.vue"]])
 /* hot reload */
 if (false) {}
 
